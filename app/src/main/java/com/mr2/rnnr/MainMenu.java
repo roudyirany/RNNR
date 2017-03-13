9dbbe1c2e9f693476f887c2038a72a8b23858273
roudyirany <roudyirany@me.com>
2017-03-13 18:16:48 +0200

    Move workout monitoring to service

--- app/src/main/java/com/mr2/rnnr/MainMenu.java
+++ app/src/main/java/com/mr2/rnnr/MainMenu.java
@@ -1,10 +1,15 @@
 package com.mr2.rnnr;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.graphics.Color;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -20,11 +25,18 @@ public class MainMenu extends AppCompatActivity {
     private Toolbar toolbar;
     private TabLayout tabLayout;
     private ViewPager viewPager;
+    LocalBroadcastManager bManager;
+    TextView speed = (TextView) findViewById(R.id.textView9);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        bManager = LocalBroadcastManager.getInstance(this);
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(RECEIVE_DATA);
+        bManager.registerReceiver(bReceiver, intentFilter);
 
         viewPager = (ViewPager) findViewById(R.id.viewpager);
         setupViewPager(viewPager);
@@ -32,6 +44,13 @@ public class MainMenu extends AppCompatActivity {
         tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(viewPager);
 
+        startService(new Intent(this, MusicService.class));
+
+    }
+
+    @Override
+    public void onDestroy(){
+        bManager.unregisterReceiver(bReceiver);
     }
 
     private void setupViewPager(ViewPager viewPager) {
@@ -41,6 +60,19 @@ public class MainMenu extends AppCompatActivity {
         adapter.addFragment(new settings(), "SETTINGS");
         viewPager.setAdapter(adapter);
     }
+
+    //Your activity will respond to this action String
+    public static final String RECEIVE_DATA = "Data received.";
+
+    private BroadcastReceiver bReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if(intent.getAction().equals(RECEIVE_DATA)) {
+                String serviceDataString = intent.getStringExtra("data");
+                speed.setText(serviceDataString);
+            }
+        }
+    };
 
     class ViewPagerAdapter extends FragmentPagerAdapter {
         private final List<Fragment> mFragmentList = new ArrayList<>();
---
+++ app/src/main/java/com/mr2/rnnr/MusicService.java
@@ -0,0 +1,212 @@
+package com.mr2.rnnr;
+
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.res.Resources;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.IBinder;
+import android.os.PowerManager;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.ActivityRecognition;
+import com.google.android.gms.location.DetectedActivity;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.text.DecimalFormat;
+
+/**
+ * Created by roudyirany on 3/13/17.
+ */
+
+public class MusicService extends Service implements SensorEventListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, ResultCallback<Status> {
+    public GoogleApiClient mApiClient;
+    private SensorManager senSensorManager;
+    private Sensor StepCounter;
+    private ActivityDetectionBroadcastReceiver mBroadcastReceiver;
+    private double stepCounter = 0;
+    private int stepMonitor = 0;
+    PowerManager powerManager;
+    PowerManager.WakeLock wakelock;
+    double stride_length = 0;
+    String activityString = "still";
+
+    // Firebase instance variables
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseUser mFirebaseUser;
+    private DatabaseReference mFirebaseDatabaseReference;
+
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+
+        mApiClient = new GoogleApiClient.Builder(this)
+                .addApi(ActivityRecognition.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+
+        mApiClient.connect();
+
+        mBroadcastReceiver = new ActivityDetectionBroadcastReceiver();
+        LocalBroadcastManager.getInstance(this).registerReceiver(mBroadcastReceiver, new IntentFilter(Constants.STRING_ACTION));
+
+
+        powerManager = (PowerManager) getSystemService(POWER_SERVICE);
+        wakelock = powerManager.newWakeLock(PowerManager.FULL_WAKE_LOCK, "wake lock");
+        wakelock.acquire();
+
+        // Initialize Firebase Auth
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseUser = mFirebaseAuth.getCurrentUser();
+        mFirebaseDatabaseReference = FirebaseDatabase.getInstance().getReference().child("users/" + mFirebaseUser.getUid() + "/speed");
+
+
+        senSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        StepCounter = senSensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+        senSensorManager.registerListener(this, StepCounter, SensorManager.SENSOR_DELAY_FASTEST);
+
+    }
+
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startid){
+        final Context context = this;
+        new CountDownTimer(90000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+            }
+
+            public void onFinish() {
+                requestActivityUpdates();
+                double distance = stride_length*stepCounter;
+                double speed = (distance/1000.0)/(10.0/3600.0);
+                DecimalFormat df = new DecimalFormat("####0.0");
+                speed = Double.valueOf(df.format(speed));
+
+                String dataString = Double.toString(speed)+" Km/h";
+                Intent RTReturn = new Intent(MainMenu.RECEIVE_DATA);
+                RTReturn.putExtra("data", dataString);
+                LocalBroadcastManager.getInstance(context).sendBroadcast(RTReturn);
+
+                stepMonitor++;
+                stepCounter = 0;
+                this.start();
+            }
+        }.start();
+
+        return START_STICKY;
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+        Sensor mySensor = event.sensor;
+
+        if(mySensor.getType() == Sensor.TYPE_STEP_COUNTER)
+        {
+            if(activityString.equals("Walking"))
+                stepCounter = stepCounter + 0.726;
+            else if(activityString.equals("Running"))
+                stepCounter = stepCounter + 1.05;
+        }
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    //Activity Recognition
+    public String getDetectedActivity(int detectedActivityType) {
+        Resources resources = this.getResources();
+        switch(detectedActivityType) {
+            case DetectedActivity.IN_VEHICLE:
+                return resources.getString(R.string.in_vehicle);
+            case DetectedActivity.ON_BICYCLE:
+                return resources.getString(R.string.on_bicycle);
+            case DetectedActivity.ON_FOOT:
+                return resources.getString(R.string.on_foot);
+            case DetectedActivity.RUNNING:
+                return resources.getString(R.string.running);
+            case DetectedActivity.WALKING:
+                return resources.getString(R.string.walking);
+            case DetectedActivity.STILL:
+                return resources.getString(R.string.still);
+            case DetectedActivity.TILTING:
+                return resources.getString(R.string.tilting);
+            case DetectedActivity.UNKNOWN:
+                return resources.getString(R.string.unknown);
+            default:
+                return resources.getString(R.string.unidentifiable_activity, detectedActivityType);
+        }
+    }
+
+    public class ActivityDetectionBroadcastReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            DetectedActivity detectedActivity = intent.getParcelableExtra(Constants.STRING_EXTRA);
+            activityString = getDetectedActivity(detectedActivity.getType());
+        }
+    }
+
+    public void requestActivityUpdates() {
+        ActivityRecognition.ActivityRecognitionApi.requestActivityUpdates(mApiClient, 0, getActivityDetectionPendingIntent()).setResultCallback(this);
+    }
+
+    private PendingIntent getActivityDetectionPendingIntent() {
+        Intent intent = new Intent(this, ActivityRecognizedService.class);
+
+        return PendingIntent.getService(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+    }
+
+    public void onResult(Status status) {
+        if (status.isSuccess()) {
+            Log.e("status: ", "Successfully added activity detection.");
+
+        } else {
+            Log.e("status: ", "Error: " + status.getStatusMessage());
+        }
+    }
+}
